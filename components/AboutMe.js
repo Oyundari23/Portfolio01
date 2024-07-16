b41e5d8fb012fda37d2fd303363cc863f89d919c
Oyundari23 <dariaa0560@gmail.com>
2024-07-16 17:03:23 +0800

    first commit

---
+++ components/AboutMe.js
@@ -0,0 +1,8 @@
+export function AboutMe() {
+    return (
+        <div>AboutMe
+            <div>Logo</div>
+            <div className="bg-red-300">Desktop</div>
+        </div>
+    );
+}
---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import { AboutMe } from "@/components/AboutMe"
+import { Header } from "@/components/Header1"
+import { Hero } from "@/components/Hero1"
+
+
+export default function Home() {
+  return (
+    <main>
+      <Header/>
+      <Hero/>
+      <AboutMe/>
+    </main>
+  );
+}
